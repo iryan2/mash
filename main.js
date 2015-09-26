0c02c336d60ad0b6156b8e84d9f2a6b0a3786ec7
Ian Ryan <iryan2@gmail.com>
2015-09-26 12:09:00 +1000

    rename project to mash

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('test');
+import calculateGross from './lib/calculateGross';
+
+function createPerson(salary) {
+    return {
+        salary: salary,
+        gross: calculateGross(salary)
+    }
+};
+
+let jess = createPerson(64000),
+    ian = createPerson(60000);
+
+console.log('Ian: ', ian.gross);
+console.log('Jess: ', jess.gross);
+console.log('Total: ', jess.gross + ian.gross);
+
+//var originalArray = [
+//    [1,2,3,4,5],
+//    ['one','two','three','four','five'],
+//    [{another: 'for good measure'}]
+//];
