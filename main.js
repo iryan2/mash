758401ff8680a050818ad15ab525cc1171c820f3
Ian Ryan <iryan2@gmail.com>
2015-09-27 20:23:43 +1000

    added basic routing

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Router, Route, Link } from 'react-router';
 import App from './lib/components/App';
 
 let rootElement = document.getElementById('root');
-React.render(
-        <App />,
-    rootElement
-);
+React.render((
+    <Router>
+        <Route path="/" component={App}>
+            <Route path="library" component={App}>
+            </Route>
+        </Route>
+    </Router>
+), rootElement );
 
