8a576358685f52e821eb9cc3291a2b90380efcaf
Ian Ryan <iryan2@gmail.com>
2015-10-16 13:15:01 +1000

    stuff

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Router, Route, Link } from 'react-router';
 import App from './lib/components/App';
+import Library from './lib/components/Library';
 
 let rootElement = document.getElementById('root');
 React.render((
     <Router>
-        <Route path="/" component={App}>
-            <Route path="library" component={App}>
-            </Route>
-        </Route>
+        <Route path="/" component={Library} />
+        <Route path="admin" component={App} />
     </Router>
 ), rootElement );
 
